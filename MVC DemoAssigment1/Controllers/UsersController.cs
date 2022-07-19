5996d1e3a9da2f167cbcbe5b8d1c1c95ae0d2781
DELL <DELL@DESKTOP-6UO4B1H>
2022-07-19 15:11:04 -0700

    Add project files.

---
+++ MVC DemoAssigment1/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_DemoAssigment1.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
